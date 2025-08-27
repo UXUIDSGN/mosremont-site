dd25599ef32299c8a41c236658c624bbf200eb66
UXUIDSGN <stasikacro@gmail.com>
2025-08-27 23:05:46 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
-        primary: '#469597',
-        secondary: '#5BA199',
-        bgLight: '#E5E3E4',
-        bgGrey: '#BBC6C8',
-        accent: '#DDBEAA',
+        primary: "#1F2937",
+        secondary: "#FBBF24",
+        accent: "#F87171",
+        light: "#F9FAFB"
       },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      }
     },
   },
   plugins: [],
